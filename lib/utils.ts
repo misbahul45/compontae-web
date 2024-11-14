6b87ed97265ed72a7a9cb08b6be04aff12f0180f
Misbahul Muttaqin <misbahulmuttaqin395@gmail.com>
2024-11-14 08:18:07 +0700

    fetch

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const sleep = (ms: number = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
+export const createSlug = (title: string) => {
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9\s-]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-')
+    .trim();
+}
