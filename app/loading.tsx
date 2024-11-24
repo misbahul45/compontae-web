3e49ac05fddc8c9ce45f069a1f8c9fcebfb5773c
Misbahul Muttaqin <misbahulmuttaqin395@gmail.com>
2024-11-24 10:29:35 +0700

    create loader page

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+    return (
+        <section className="w-full h-screen flex items-center justify-center">
+            <div className="loading-wave">
+                <div className="loading-bar"></div>
+                <div className="loading-bar"></div>
+                <div className="loading-bar"></div>
+                <div className="loading-bar"></div>
+            </div>
+        </section>
+    )
+}
