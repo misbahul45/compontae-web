5bd3d13e79f021bd65259b5f4c8bb0b65aae8bfd
Misbahul Muttaqin <misbahulmuttaqin395@gmail.com>
2024-11-30 12:53:25 +0700

    push

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -2,31 +2,18 @@ import ShowLengthData from '@/components/dashboard/ShowLengthData';
 import UserAndCommentBar from '@/components/dashboard/UserAndCommentBar';
 import CommentWordCloud from '@/components/dashboard/worldCloudComment';
 import { Separator } from '@/components/ui/separator';
-import React from 'react';
-import { getLengthAllComment } from '@/actions/comment-action';
-import { getLengthAllPosts } from '@/actions/post-action';
-import { getLengthAllUser } from '@/actions/user-action';
+import prisma from '@/lib/db';
 
-export async function getServerSideProps() {
-  const lengthPost = await getLengthAllPosts();
-  const lengthUser = await getLengthAllUser();
-  const lengthComment = await getLengthAllComment();
+const Page = async () => {
+  const lengthPost = await prisma.post.count();
+  const lengthUser = await prisma.user.count();
+  const lengthComment = await prisma.comment.count();
 
-  return {
-    props: {
-      lengthPost,
-      lengthUser,
-      lengthComment,
-    },
-  };
-}
-
-const Page = ({ lengthPost, lengthUser, lengthComment }: { lengthPost: number; lengthUser: number; lengthComment: number }) => {
   return (
     <div className='pt-20 pb-24'>
       <h1 className='text-center lg:text-7xl md:text-5xl sm:text-4xl text-2xl font-bold md:mb-4 mb-2'>Dashboard</h1>
       <ShowLengthData
-        lengthComment={lengthComment}
+        lengthComment={lengthComment }
         lengthPost={lengthPost}
         lengthUser={lengthUser}
       />
