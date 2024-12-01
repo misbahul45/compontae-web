3833a1c3eff83d1fd3677b7d418f7ff8579fc49e
Misbahul Muttaqin <misbahulmuttaqin395@gmail.com>
2024-12-02 01:25:57 +0700

    update npm

--- components/Home/Item.tsx
+++ components/Home/Item.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { motion } from "motion/react"
 interface Props {
     title: string  
     children?: React.ReactNode
@@ -7,10 +6,10 @@ interface Props {
 
 const Item = ({title, children}: Props) => {
   return (
-    <motion.div initial={{ scale:0 }} whileInView={{ scale:1, transition:{duraton:1 } }} className='flex flex-col text-center w-full h-48 items-center justify-center gap-4 rounded-md border-2 shadow-md hover:shadow-lg hover:shadow-slate-700/40 transition-all duration-100'>
+    <div className='flex flex-col text-center w-full h-48 items-center justify-center gap-4 rounded-md border-2 shadow-md hover:shadow-lg hover:shadow-slate-700/40 transition-all duration-100'>
         {children}
         <h1 className='md:text-lg sm:text-md sm:text-sm text-xs font-semibold'>{title}</h1>
-    </motion.div>
+    </div>
   )
 }
 
